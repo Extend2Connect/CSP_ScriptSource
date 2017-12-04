98f8b5ca84f918d8fb44637d3f90e52cae9936bd
peter-kehl <peter.kehl@gmail.com>
2017-12-04 12:34:52 +1100

    Initial

---
+++ background.js
@@ -0,0 +1,9 @@
+"use strict";
+
+browser.runtime.onMessageExternal.addListener( (message, sender, sendResponse)=>{
+    console.error( `onMessageExternal handler invoked: ${message}, ${sender}, ${sendResponse}`);
+    sendResponse( browser.runtime.getURL('') );
+});
+
+console.error( `CSP_MozExtension_ScriptSource loaded from URL ${window.document.currentScript.src}`);
+
